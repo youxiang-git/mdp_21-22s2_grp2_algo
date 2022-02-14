700f97dceb4f28512ac4cf3ff8fe21e2a5023656
youxiang-git <chaiyouxiang@gmail.com>
2022-02-14 23:10:12 +0800

    fixed hamiltonian path

--- pygame_test/tempCodeRunnerFile.py
+++ pygame_test/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-RED = (255, 0, 0)
+def main():
+#     c_list = [(2, 16), (6, 16), (3, 4), (12, 8), (15, 2), (15, 16)]
+#     tsp_p = calc_TSP(c_list)
+#     print(tsp_p)
+
+# main()
--- pygame_test/tsp_approx.py
+++ pygame_test/tsp_approx.py
@@ -3,8 +3,7 @@ import matplotlib
 import numpy as np
 
 def calc_TSP(node_c):
-    g1 = nx.complete_graph(len(node_c))
-    # print(g1.nodes)
+    g1 = nx.complete_graph(6)
     for node, node2 in g1.edges:
         # print(node_c[node], node_c[node2])
         # print(node, node2)
@@ -14,19 +13,21 @@ def calc_TSP(node_c):
         # print(xy)
         xy = np.sum(xy)
         g1[node][node2]['weight'] = xy
-        # print(g1[node][node2]['weight'])    
+        # print(g1[node][node2]['weight'])
+
+    # print(g1[0])
+
 
     # nx.draw(g1, node_c, with_labels = True)
     # labels = nx.get_edge_attributes(g1,'weight')
     # nx.draw_networkx_edge_labels(g1,node_c,edge_labels=labels)
-
-    tsp_path = nx.approximation.traveling_salesman_problem(g1, weight = 'weight')
-    print(tsp_path)
+    tsp_path = nx.approximation.traveling_salesman_problem(g1, weight = 'weight', cycle = True, method = nx.algorithms.approximation.traveling_salesman.greedy_tsp)
     return tsp_path
 
 
 # def main():
-#     c_list = [(2, 16), (4, 5), (7, 12), (14, 7), (13, 2), (14, 14)]
-#     calc_TSP(c_list)
+#     c_list = [(2, 16), (5, 2), (3, 9), (11, 7), (16, 4), (16, 13)]
+#     tsp_p = calc_TSP(c_list)
+#     print(tsp_p)
 
 # main()
